66c17b76e5568cabc2d9e6c875e08473534df540
lvjian700 <lvjian700@gmail.com>
2013-05-19 10:26:35 +0800

    add watch feature

--- bin/less2css.js
+++ bin/less2css.js
@@ -1,19 +1,36 @@
 #!/usr/local/bin/node
 var exec = require('child_process').exec,
-    path = require('path')
+    path = require('path'),
+	fs = require('fs')
 
 var public_dir = path.join(__dirname, "..", "public");
 
 var less_dir = path.join(public_dir, "less", "base.less");
 var css_dir = path.join(public_dir, "css", "base.css");
 
-var cmd = ['lessc ', less_dir, ' > ', css_dir].join('');
 
-exec(cmd, {encoding: 'utf-8'}, 
-	function(error, stdout, stderr) {
-		if(error !== null) {
-			console.log(error);
-			return;
-		}
-		console.log(stdout);
+
+function compile_less(input_file, output_file) {
+	var cmd = ['lessc ', input_file, ' > ', output_file].join('');
+	exec(cmd, {encoding: 'utf-8'}, 
+		function(error, stdout, stderr) {
+			if(error !== null) {
+				console.log(error);
+				return;
+			}
+			console.log(stdout);
+		});
+}
+
+console.log('compile ' + less_dir + ' once...');
+compile_less(less_dir, css_dir);
+
+console.log('watching file ...');
+fs.watchFile(less_dir, { 
+		persistent: true, 
+		interval: 1000 // 1 sec
+	}, 
+	function(curr, prev) {
+		console.log('the file changed, compile ...');
+		compile_less(less_dir, css_dir);
 	});
